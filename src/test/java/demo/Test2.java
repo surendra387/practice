18b0c6b9be309f5084bb2ab605d0fddb0879ae88
surendra387 <surendrapasala387@gmail.com>
2024-08-10 20:59:02 +0530

    second commit

---
+++ src/test/java/demo/Test2.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class Test2 {
+public static void main(String[] args) {
+	System.out.println("first test script");
+	System.out.println("second test script test script");
+
+}
+}
